b8f95c14a05c4068a872304ed3be69f8dc4dbb1f
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2024-02-22 21:04:01 -0500

    Add files via upload

---
+++ earningcalender_schema.sql
@@ -0,0 +1,12 @@
+create table EarningCalender(
+symbol VARCHAR not null,
+name VARCHAR not null,
+reportDate Date,
+fiscalDate Date,
+estimate Float,
+currency VARCHAR);
+
+select * from earningcalender;
+
+
+
